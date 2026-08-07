3b100387c23a1432ca7d1dbf610b29897135aa35
shreesanwriyacyber <shreesanwriyacyber@gmail.com>
2026-08-07 16:29:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 console.log("OnlineSevaUpdate Loaded Successfully");
+function searchWebsite() {
+
+    let value = document.getElementById("searchInput").value.toLowerCase().trim();
+
+    if (value.includes("job") || value.includes("police") || value.includes("vacancy")) {
+
+        window.location.href = "jobs.html";
+
+    }
+
+    else if (value.includes("result")) {
+
+        window.location.href = "results.html";
+
+    }
+
+    else if (value.includes("admit")) {
+
+        window.location.href = "admit-card.html";
+
+    }
+
+    else if (value.includes("yojana")) {
+
+        window.location.href = "yojana.html";
+
+    }
+
+    else if (value.includes("samagra")) {
+
+        window.location.href = "samgra.html";
+
+    }
+
+    else if (value.includes("csc")) {
+
+        window.location.href = "csc.html";
+
+    }
+
+    else if (value == "") {
+
+        alert("Please enter something to search.");
+
+    }
+
+    else {
+
+        alert("No matching page found.");
+
+    }
+
+}
