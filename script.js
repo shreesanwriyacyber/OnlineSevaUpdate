6674093ae152df49ff0245aa5a2ae09a1a097268
shreesanwriyacyber <shreesanwriyacyber@gmail.com>
2026-08-07 17:06:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,27 +54,19 @@ function searchWebsite() {
 }
 // Back To Top Button
 
-let topButton = document.getElementById("topBtn");
+const topButton = document.getElementById("topBtn");
 
-window.onscroll = function () {
-
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
         topButton.style.display = "block";
-
     } else {
-
         topButton.style.display = "none";
-
     }
-
-};
+});
 
 function topFunction() {
-
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
-
 }
