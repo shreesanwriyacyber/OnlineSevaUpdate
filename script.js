68f8a42c2b896af0bd331db925dc11062e5327f0
shreesanwriyacyber <shreesanwriyacyber@gmail.com>
2026-08-07 16:58:28 +0530

    Implement back to top button functionality
    Add functionality for a back to top button that appears after scrolling down 300 pixels.

--- script.js
+++ script.js
@@ -52,3 +52,29 @@ function searchWebsite() {
     }
 
 }
+// Back To Top Button
+
+let topButton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+
+        topButton.style.display = "block";
+
+    } else {
+
+        topButton.style.display = "none";
+
+    }
+
+};
+
+function topFunction() {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
